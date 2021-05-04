0e38afa3c06e9dace73281df28211b10461049b8
James C <JCarr@qa.com>
2021-05-04 15:42:26 +0000

    Commiting files for end of the day

--- tutorial_project/create.py
+++ tutorial_project/create.py
@@ -1,19 +1,3 @@
-from app import db, Customers, Products, Army, Orders
+from application import db
 
-db.drop_all()
 db.create_all()
-
-customer_1 = Customers(name='Alicia')
-product_1 = Products(name='Borderlands',price=29.99)
-order_1 = Orders(customer_id=1,product_id=1)
-
-db.session.add(customer_1)
-db.session.add(product_1)
-db.session.add(order_1)
-db.session.commit()
-
-necron = Army(army_name='Necrons', faction ='Xeno', alignment='Evil')
-
-
-db.session.add(necron)
-db.session.commit()
